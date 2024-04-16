299d66fd08b2af2003af754c2adf828105ed2e94
todermann2 <156466663+todermann2@users.noreply.github.com>
2024-04-15 20:25:26 -0500

    Update CISMain.py

--- CISMain.py
+++ CISMain.py
@@ -11,7 +11,8 @@ def main():
       metric = input("Enter the original metric type for your length: ")
       convert = input("Enter the metric to convert to: ")
       length = float(input("Enter the length: "))
-      converter.use_convert(metric, convert, length)
+      len_result = converter.use_convert(metric, convert, length)
+        print(f"{len_result:.4f}")
 
     if class_selection == "temp":
         converter = TemperatureConverter
@@ -23,14 +24,16 @@ def main():
         metric = input("Enter the original metric type for your area: ")
         convert = input("Enter the metric to convert to: ")
         area = float(input("Enter the area: "))
-        converter.use_convert(metric, convert, area)
+        area_result = converter.use_convert(metric, convert, area)
+        print(f"{area_result:.4f}")
     
     if class_selection == "volume":
         converter = VolumeConverter()
         metric = input("Enter the original metric type for your volume: ")
         convert = input("Enter the metric to convert to: ")
         volume = float(input("Enter the volume: "))
-        converter.use_convert(metric, convert, volume)
+        volume_result = converter.use_convert(metric, convert, volume)
+        print(f"{volume_result:.4f}")
 
 if __name__ == "__main__":
     main() 
