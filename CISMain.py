a18d795aef553c9d8cf622afa8744036fbc2f166
Juvy <156458218+juvylee@users.noreply.github.com>
2024-04-16 15:07:41 -0500

    Update CISMain.py

--- CISMain.py
+++ CISMain.py
@@ -94,7 +94,10 @@ def main():
 			volume = float(input("Enter the volume: "))
 			
 			converted_volume = converter.use_convert(metric, convert, volume)
-			print(f"{converted_volume:.2f} {convert}")
+			if converted_volume > 1:
+				print(f"{converted_volume:.2f} {convert}s")
+			if converted_volume <= 1:
+				print(f"{converted_volume:.2f} {convert}")
 			conversions.append((metric, convert, volume, converted_volume))
 
 		elif class_selection == "exit":
